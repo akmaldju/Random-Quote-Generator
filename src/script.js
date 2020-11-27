d42751528e10e1beb3656cc0640ca6e5e4393e39
Akmal Djumakhodjaev <33540033+akmaldju@users.noreply.github.com>
2020-11-28 01:12:41 +0800

    Refactor all files

--- src/script.js
+++ src/script.js
@@ -1,13 +1,14 @@
 const colors = ['#009688', '#00BCD4', '#3F51B5', '#673AB7', '#9C27B0', '#DB4437', '#FF9800', '#00BCD4', '#00FA9A'];
 
 let lang = 'en';
+let forismaticUrl = `https://api.forismatic.com/api/1.0/?method=getQuote&lang=${lang}&format=jsonp&jsonp=?`;
 
 $(document).ready(() => {
-  $.getJSON(`https://api.forismatic.com/api/1.0/?method=getQuote&lang=${lang}&format=jsonp&jsonp=?`)
+  $.getJSON(forismaticUrl)
     .done(update).fail(handleErr);
 
   $('#newquote').click(() => {
-    $.getJSON(`https://api.forismatic.com/api/1.0/?method=getQuote&lang=${lang}&format=jsonp&jsonp=?`)
+    $.getJSON(forismaticUrl)
       .done(update).fail(handleErr);
   });
 
@@ -21,21 +22,24 @@ $(document).ready(() => {
       $('#set a').text('RUS');
     }
   
-    $.getJSON(`https://api.forismatic.com/api/1.0/?method=getQuote&lang=${lang}&format=jsonp&jsonp=?`)
+    $.getJSON(forismaticUrl)
       .done(update).fail(handleErr);
   });
 });
 
 const update = (quote) => {
+  const quoteText = JSON.stringify(quote.quoteText);
+  const quoteAuthor = JSON.stringify(quote.quoteAuthor);
+  
   $('body').css('background-color', colors[Math.round(Math.random() * 8) + 1]);
-  $('#thetarget').html(JSON.stringify(quote.quoteText));
-  $('#quote h5').html(`- <a href='https://www.google.com/search?q=${JSON.stringify(quote.quoteAuthor).replace('"', '').replace('"', '')}' target='_blank'>${JSON.stringify(quote.quoteAuthor).replace('"', '').replace('"', '')}</a>`);
+  $('#thetarget').html(quoteText);
+  $('#quote h5').html(`- <a href='https://www.google.com/search?q=${quoteAuthor.replace('"', '').replace('"', '')}' target='_blank'>${quoteAuthor.replace('"', '').replace('"', '')}</a>`);
 
   /*$('#tweet').html('<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=' + JSON.stringify(quote.quoteText) + ' -' + JSON.stringify(quote.quoteAuthor) +'" data-size="large"></a>');
     $('#tweet').html(
       '<a class=\"twitter-share-button\" href=\"https://twitter.com/intent/tweet?text=' + JSON.stringify(quote.quoteText) + '\" data-size=\"large\"></a>'
     );*/
-  let message = `${JSON.stringify(quote.quoteText)}   -  ${JSON.stringify(quote.quoteAuthor)}`;
+  let message = `${quoteText}   -  ${quoteAuthor}`;
   $('#tweet').html(
     `<a class='twitter-share-button' href='https://twitter.com/intent/tweet?text=${message} data-size='large'></a>`
   );
